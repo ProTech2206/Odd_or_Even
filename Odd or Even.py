0d0564505644241f3e9e8b5dd7bf41757cf30efe
ProTech <71920685+ProTech2206@users.noreply.github.com>
2020-09-29 13:41:39 +0530

    Add files via upload

---
+++ Odd or Even.py
@@ -0,0 +1,13 @@
+print('ODD OR EVEN?')
+print('Tᴏᴏʟ Oᴡɴᴇʀ: PʀᴏTᴇᴄʜ2206')
+print('Sᴄʀɪᴘᴛ ᴍᴀᴅᴇ ʙʏ: Sɪᴀ')
+print('Eᴅɪᴛᴇᴅ ʙʏ: AᴍᴀᴛᴜᴇʀCᴏᴅᴇʀ')
+print('YᴏᴜTᴜʙᴇ: AᴍᴀᴛᴜᴇʀCᴏᴅᴇʀ PʀᴏTᴇᴄʜ')
+print('      ')
+print('|______________________|')
+print('              ')
+num=int(input("Enter a Number:"))
+if(num%2==0):
+    print('{0} is an even number'.format(num))
+else:
+    print('{0} is an odd number'.format(num))
